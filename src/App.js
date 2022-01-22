92fe974b444febf01731fd5ced9f29bf0f7a4b00
Jan Arcalas <jan.arcalas@recro.com>
2022-01-21 20:56:27 -0700

    dev: add title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App () {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
-          new message
+          Welcome to Smokey Rewards
         </p>
         <a
           className='App-link'
