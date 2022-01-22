aea26fdf13577a8c59ce28999ff5efd2cd327366
Jan Arcalas <jan.arcalas@recro.com>
2022-01-21 18:27:54 -0700

    fix: change port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ fastify.get('/', async (request, reply) => {
 
 const start = async () => {
   try {
-    await fastify.listen(3000, '0.0.0.0')
+    await fastify.listen(8080, '0.0.0.0')
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
