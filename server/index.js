9851b0a75cb33e0ea74d166627854570de0000f8
Jan Arcalas <jan.arcalas@recro.com>
2022-01-24 18:56:12 -0700

    dev: redirect

--- server/index.js
+++ server/index.js
@@ -8,8 +8,6 @@ const fastify = require('fastify')({
   logger: true
 })
 
-fastify.register(httpsRedirect)
-
 fastify.register(require('fastify-static'), {
   root: path.resolve(__dirname, '../', 'build')
 })
@@ -18,13 +16,4 @@ fastify.get('/', async (request, reply) => {
   reply.sendFile('index.html')
 })
 
-const start = async () => {
-  try {
-    const port = process.env.PORT
-    await fastify.listen(port, '0.0.0.0')
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-}
-start()
+fastify.register(httpsRedirect, { httpPort: process.env.PORT })
