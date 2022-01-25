36645a62ef4b50a2cae28b82d01690043da1c3be
Jan Arcalas <jan.arcalas@recro.com>
2022-01-25 15:27:51 -0700

    fix: test data

--- server/routes/v1/members.js
+++ server/routes/v1/members.js
@@ -2,7 +2,7 @@
 module.exports = function (fastify, _opts, done) {
   fastify.get('/members', {
     handler: function (request, reply) {
-      if (request.user.permissions.includes('read:members')) { reply.send(request.user) }
+      if (request.user.permissions.includes('read:members')) { reply.send([{ name: 'test' }]) }
     },
     preValidation: fastify.authenticate
   })
